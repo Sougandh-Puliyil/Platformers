9b7083d96102a01ee439e350f4ace68af98a3493
Sougandh <sougandhpuliyil@gmail.com>
2026-05-23 23:50:54 +0530

    misc

---
+++ Scripts/tutorials.gd
@@ -0,0 +1,13 @@
+extends Area2D
+#@onready var label: Label = $CollisionShape2D/label
+#
+#func _ready():
+	#label.hide()
+#
+#func _on_body_entered(body):
+	#if body.name == "player":
+		#label.show()
+#
+#func _on_body_exited(body):
+	#if body.name == "player":
+		#label.hide()
