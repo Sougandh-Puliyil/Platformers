04c482a930f128de556887892a74500e9e1ee903
akashp <akashswapnam2007@gmail.com>
2026-05-24 15:18:52 +0530

    Enemy1

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 const SPEED = 100.0
 const JUMP_VELOCITY = -300.0
 
@@ -17,6 +16,7 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
